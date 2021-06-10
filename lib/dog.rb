2b12ad21ab759871ca656138ae3010664fd28ba0
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-06-10 13:35:39 +0000

    puppy lab done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
 # Add your code here
+
+require "pry"
+
+class Dog
+    attr_accessor :name
+
+    @@all = []
+
+    def save
+        @@all << self
+    end
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        self.all.clear
+    end
+
+    def self.print_all
+        self.all.each{|dog| puts dog.name}
+        binding.pry
+    end
+
+
+
+end
+
